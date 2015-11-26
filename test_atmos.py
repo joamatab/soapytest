653e11a66a1e65d7246506de33d3db4c604fc9f1
Andrew Reeves <andrewpaulreeves@gmail.com>
2015-11-26 09:35:49 +0000

    added a test of the Noll Zernike spectrum

--- test_atmos.py
+++ test_atmos.py
@@ -1,4 +1,46 @@
-from soapy import atmos, confParse
+import numpy
 
-from aotools import circle
+from soapy import atmosphere
+
 from aotools.circle import zernike
+
+
+def testNollSpectrum(nZerns, nScrns, scrnSize, subScrnSize, r0):
+
+    Zs = zernike.zernikeArray(nZerns+1, subScrnSize)[1:]
+
+    Zs.shape = nZerns, subScrnSize*subScrnSize
+
+    subsPerScrn = scrnSize/subScrnSize
+    zCoeffs = numpy.zeros((nZerns, nScrns*(subsPerScrn**2)))
+
+    i = 0
+    for n in range(nScrns):
+        # Make one big screen
+        scrn = atmosphere.ft_phase_screen(
+                r0, scrnSize, 1./subScrnSize, 100., 0.01)
+
+        # Pick out as many sub-scrns as possible to actually test
+        for x in range(subsPerScrn):
+            for y in range(subsPerScrn):
+                subScrn = scrn[
+                        x*subScrnSize: (x+1)*subScrnSize,
+                        y*subScrnSize: (y+1)*subScrnSize]
+                subScrn = subScrn.reshape(subScrnSize*subScrnSize)
+
+                # Dot with zernikes to get powerspec
+                zCoeffs[:, i] = (Zs*subScrn).sum(1)
+                i+=1
+
+    return zCoeffs
+
+if __name__=="__main__":
+
+    # Number of scrns to average over
+    nScrns = 500
+    r0 = 1. # R0 value to use in test
+    nZerns = 50
+    subScrnSize = 256
+    scrnSize = 512
+
+    zCoeffs = testNollSpectrum(nZerns, nScrns, scrnSize, subScrnSize, r0)
