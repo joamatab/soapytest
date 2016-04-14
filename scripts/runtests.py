8351cf29c0f3c0e43855636ef42c9e40d34b7a70
Andrew Reeves <andrewpaulreeves@gmail.com>
2016-04-14 19:12:26 +0100

    make build script python

--- scripts/runtests.py
+++ scripts/runtests.py
@@ -1,20 +1,24 @@
 #!/usr/bin/env python
 import os
 import sys
+import subprocess
+
 MAC_CONDA = "https://repo.continuum.io/miniconda/Miniconda2-latest-MacOSX-x86_64.sh"
 LINUX_CONDA = "https://repo.continuum.io/miniconda/Miniconda2-latest-MacOSX-x86_64.sh"
 
-if
-CONDA =
+if sys.platform=="darwin":
+    CONDA_LINK = "https://repo.continuum.io/miniconda/Miniconda2-latest-MacOSX-x86_64.sh"
+elif sys.platform == "linux2":
+    CONDA_LINK = "https://repo.continuum.io/miniconda/Miniconda2-latest-MacOSX-x86_64.sh"
 
 
-os.system("wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh")
-chmod +x miniconda.sh
-./miniconda.sh -b -p $(pwd)/miniconda
-export PATH=$(pwd)/miniconda/bin:$PATH
-conda update --yes conda
-conda install --yes  nose coverage numpy numba
-python setup.py install
+sys.subprocess(["wget", CONDA_LINK, "-O", "miniconda.sh"])
+# chmod +x miniconda.sh
+# ./miniconda.sh -b -p $(pwd)/miniconda
+# export PATH=$(pwd)/miniconda/bin:$PATH
+# conda update --yes conda
+# conda install --yes  nose coverage numpy numba
+# python setup.py install
 
 
 # export PATH=/opt/anaconda2/bin:$PATH
