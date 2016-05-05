65df516b39f3f3a4f82b62653ae2b43a0f4efbdd
Andrew Reeves <andrewpaulreeves@gmail.com>
2016-05-05 13:49:51 +0100

    fixes to grad wfs tests

--- soapytest/wfs/gradpixelscale.py
+++ soapytest/wfs/gradpixelscale.py
@@ -68,7 +68,7 @@ class GradPixelScale(object):
 
         aTilt = self.tilt.copy()*tiltAmp
         slopes = self.wfs.frame(aTilt)
-        measuredTilt = -1*slopes[self.wfs.activeSubaps:].mean()
+        measuredTilt = -1*slopes[:self.wfs.activeSubaps].mean()
 
         return measuredTilt
 
@@ -85,6 +85,7 @@ class GradPixelScale(object):
 
         self.measuredTilts = numpy.zeros((tiltAmps.shape[0]))
         for iA, A in enumerate(tiltAmps):
+            print("tiltAmp: {}nm".format(A))
             self.measuredTilts[iA] = self.getSlopeFromTilt(A)
             
         return self.tilts_asec, self.measuredTilts
--- test/testgradwfs.py
+++ test/testgradwfs.py
@@ -10,7 +10,7 @@ from soapytest.wfs import gradpixelscale
 def testPixelScale():
     pxlScales, measuredPxlScales = gradpixelscale.testPixelScale()
 
-    print("Pixel Scales: \t\t{}".format(pxlScales))
-    print("Measured Pixel Scales: \t{}".format(measuredPxlScales))
+    print("Tilts: \t\t{}".format(pxlScales))
+    print("Measured Tilts: \t{}".format(measuredPxlScales))
 
     assert numpy.allclose(pxlScales, measuredPxlScales, rtol=0.1)
