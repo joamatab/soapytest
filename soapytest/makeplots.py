546daed56be06126b3ca8e007698645721a5a07d
Andrew Reeves <andrewpaulreeves@gmail.com>
2016-07-11 09:52:44 +0100

    add lineofsight plots

--- soapytest/makeplots.py
+++ soapytest/makeplots.py
@@ -5,6 +5,7 @@ from soapytest import plotting
 def makePlots(outputdir=None):
     # Atmospehre Plots
     plotting.plotatmos.plotZernSpec(outputdir)
+    plotting.plotatmos.plotPhaseVariance(outputdir)
 
     # SH WFS plots
     plotting.plotshwfs.plotSHPixelScale(outputdir)
@@ -13,6 +14,10 @@ def makePlots(outputdir=None):
     # Gradient wfs plots
     plotting.plotgradwfs.plotGradPixelScale(outputdir)
     plotting.plotgradwfs.plotGradSlopeVariance(outputdir)
+
+    # LOS plots
+    plotting.plotlineofsight.plotPhaseVariance(outputdir)
+    plotting.plotlineofsight.plotPhaseVariance_vs_scrnSize(outputdir)
 
 if __name__=='__main__':
     makePlots()
